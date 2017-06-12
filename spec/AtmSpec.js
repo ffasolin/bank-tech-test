2c6ef1295ca5b61d0c0437cb6665aeb004c722a4
Filipe Fasolin <ffasolin@gmail.com>
2017-06-12 23:01:37 +0100

    Adds ability to make deposit

--- spec/AtmSpec.js
+++ spec/AtmSpec.js
@@ -3,8 +3,16 @@ const Atm = require('../src/Atm.js')
 describe('Atm', function() {
   var atm;
 
-  it('displays an initial balance of 0', function() {
+  beforeEach(function() {
     atm = new Atm();
+  });
+
+  it('displays an initial balance of 0', function() {
     expect(atm.displayBalance()).toBe(0);
+  });
+
+  it('makes a deposit into balance', function() {
+    atm.makeDeposit(500)
+    expect(atm.displayBalance()).toBe(500);
   });
 });
--- src/Atm.js
+++ src/Atm.js
@@ -2,6 +2,10 @@ function Atm(balance = 0) {
   this._balance = balance;
 }
 
+Atm.prototype.makeDeposit = function(amount) {
+  this._balance += amount;
+};
+
 Atm.prototype.displayBalance = function() {
   return this._balance;
 };
