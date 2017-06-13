b9cffc2053a440a8d383af97b2a0d6b7884d69fe
Filipe Fasolin <ffasolin@gmail.com>
2017-06-13 23:42:12 +0100

    TransactionStyler called from Atm

--- src/Atm.js
+++ src/Atm.js
@@ -1,18 +1,22 @@
 const Transaction = require('../src/Transaction.js');
+const TransactionStyler = require('../src/TransactionStyler.js');
 
 function Atm(balance = 0) {
   this._balance = balance;
   this.transaction = new Transaction();
+  this.styler = new TransactionStyler();
 }
 
 Atm.prototype.deposit = function(date, amount) {
   this._balance += amount;
   this.transaction.saveDeposit([date, amount, this._balance]);
+  this.styler.styledDeposit(this.transaction.depositsList);
 };
 
 Atm.prototype.withdraw = function(date, amount) {
   this._balance -= amount;
   this.transaction.saveWithdrawal([date, amount, this._balance]);
+  this.styler.styledWithdrawal(this.transaction.withdrawalsList);
 };
 
 Atm.prototype.displayBalance = function() {
