5ea984079cc404efbc0188391bdf83ba731ad660
Filipe Fasolin <ffasolin@gmail.com>
2017-06-13 16:31:25 +0100

    Fixes long lines in TransactionStyler

--- src/TransactionStyler.js
+++ src/TransactionStyler.js
@@ -5,13 +5,15 @@ function TransactionStyler() {
 TransactionStyler.prototype.styledDeposit = function(deposit) {
   var flatDeposit = [].concat.apply([], deposit);
 
-  this.transactions.push(`${flatDeposit[0]} || ${flatDeposit[1].toFixed(2)} || || ${flatDeposit[2].toFixed(2)}`);
+  this.transactions.push(`${flatDeposit[0]} || ${flatDeposit[1].toFixed(2)}` +
+          ` || || ${flatDeposit[2].toFixed(2)}`);
 };
 
 TransactionStyler.prototype.styledWithdrawal = function(withdraw) {
   var flatWithdraw = [].concat.apply([], withdraw);
 
-  this.transactions.push(`${flatWithdraw[0]} || || ${flatWithdraw[1].toFixed(2)} || ${flatWithdraw[2].toFixed(2)}`);
+  this.transactions.push(`${flatWithdraw[0]} || || ${flatWithdraw[1].toFixed(2)}` +
+          ` || ${flatWithdraw[2].toFixed(2)}`);
 };
 
 module.exports = TransactionStyler;
