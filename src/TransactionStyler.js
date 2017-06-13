75073be6b636f710c9c36bc23133be7103eea752
Filipe Fasolin <ffasolin@gmail.com>
2017-06-13 14:52:58 +0100

    Adds styler function for withdrawals

--- src/TransactionStyler.js
+++ src/TransactionStyler.js
@@ -6,4 +6,8 @@ TransactionStyler.prototype.styledDeposit = function(deposit) {
   this.transactions.push(`${deposit[0]} || ${deposit[1].toFixed(2)} || || ${deposit[2].toFixed(2)}`);
 };
 
+TransactionStyler.prototype.styledWithdrawal = function(withdraw) {
+  this.transactions.push(`${withdraw[0]} || || ${withdraw[1].toFixed(2)} || ${withdraw[2].toFixed(2)}`);
+};
+
 module.exports = TransactionStyler;
