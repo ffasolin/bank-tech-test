9d1bc2d4d0ac2c841a5025ab4d4fd10138193c30
Filipe Fasolin <ffasolin@gmail.com>
2017-06-13 11:42:17 +0100

    Updates Atm to push transfers into Transactions arrays

--- spec/AtmSpec.js
+++ spec/AtmSpec.js
@@ -1,10 +1,12 @@
 const Atm = require('../src/Atm.js');
+const Transaction = require('../src/Transaction.js');
 
 describe('Atm', function() {
-  var atm;
+  var atm, transaction;
 
   beforeEach(function() {
     atm = new Atm();
+    transaction = new Transaction();
   });
 
   it('displays an initial balance of 0', function() {
@@ -12,12 +14,22 @@ describe('Atm', function() {
   });
 
   it('makes a deposit', function() {
-    atm.deposit(500);
+    atm.deposit('13/06/2017', 500);
     expect(atm.displayBalance()).toBe(500);
   });
 
   it('makes a withdrawal', function() {
-    atm.withdraw(500);
+    atm.withdraw('13/06/2017', 500);
     expect(atm.displayBalance()).toBe(-500);
+  });
+
+  it('deposits are stored into deposits array', function() {
+    atm.deposit('13/06/2017', 500);
+    expect(atm.transaction.depositsList).toContain(['13/06/2017', 500, 500]);
+  });
+
+  it('withdrawals are stored into withdrawals array', function() {
+    atm.withdraw('13/06/2017', 500);
+    expect(atm.transaction.withdrawalsList).toContain(['13/06/2017', 500, -500]);
   });
 });
--- src/Atm.js
+++ src/Atm.js
@@ -1,13 +1,18 @@
+const Transaction = require('../src/Transaction.js');
+
 function Atm(balance = 0) {
   this._balance = balance;
+  this.transaction = new Transaction();
 }
 
-Atm.prototype.deposit = function(amount) {
+Atm.prototype.deposit = function(date, amount) {
   this._balance += amount;
+  this.transaction.saveDeposit([date, amount, this._balance]);
 };
 
-Atm.prototype.withdraw = function(amount) {
+Atm.prototype.withdraw = function(date, amount) {
   this._balance -= amount;
+  this.transaction.saveWithdrawal([date, amount, this._balance]);
 };
 
 Atm.prototype.displayBalance = function() {
