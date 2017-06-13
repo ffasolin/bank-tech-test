9f25af78fb1ad856df04d0230ec280a51e483634
Filipe Fasolin <ffasolin@gmail.com>
2017-06-13 15:42:32 +0100

    StatementPrinter prints full Statement

--- spec/StatementPrinterSpec.js
+++ spec/StatementPrinterSpec.js
@@ -1,10 +1,22 @@
 const StatementPrinter = require('../src/StatementPrinter.js');
 
 describe('StatementPrinter', function() {
-  var statement;
+  var finalStatement, statement, transactions;
 
   it('has a statement header', function() {
     statement = new StatementPrinter();
     expect(statement.header).toBe('date || credit || debit || balance');
+  });
+
+  it('returns styled strings from array', function() {
+    statement = new StatementPrinter();
+    transactions = [['10/01/2012 || 1000.00 || || 1000.00'],
+                    ['13/01/2012 || 2000.00 || || 3000.00'],
+                    ['14/01/2012 || || 500.00 || 2500.00']];
+    finalStatement = 'date || credit || debit || balance' + '\n' +
+                     '14/01/2012 || || 500.00 || 2500.00' + '\n' +
+                     '13/01/2012 || 2000.00 || || 3000.00' + '\n' +
+                     '10/01/2012 || 1000.00 || || 1000.00';
+    expect(statement.fullStatement(transactions)).toEqual(finalStatement);
   });
 });
--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -2,4 +2,10 @@ function StatementPrinter() {
   this.header = 'date || credit || debit || balance';
 }
 
+StatementPrinter.prototype.fullStatement = function(transactions) {
+  var reversedTransactions = [].concat(transactions).reverse();
+
+  return [].concat.apply(this.header, [reversedTransactions]).join('\n');
+};
+
 module.exports = StatementPrinter;
